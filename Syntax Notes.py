df7ea3b131e2c53dcf2f013e69c367c9010bef0e
Imaginality <mattie168@gmail.com>
2016-08-10 15:10:58 -0400

    Notes on Python Syntax

---
+++ Syntax Notes.py
@@ -0,0 +1,18 @@
+'''
+for i in ___
+    (for loop)goes through each individual letter or number in string or list
+len({list})
+    counts the amount of elements in the string
+while___:
+    ___ is a condition and repeats what is below while the condition was still met
+{list}.append(___)
+    adds strings to the end of the list or inputs full lists inside list
+{list}.extend(___)
+    adds strings by individual letter or lists by elements
+{list}+something
+    adds something to the list
+chr()
+    Numbers to Letters
+ord()
+    Letters to Numbers
+'''
